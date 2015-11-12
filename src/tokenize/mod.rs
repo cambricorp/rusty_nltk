ee8133944a81f067732c74ec06fa89bf70172456
Mike Recachinas <mgr3yp@virginia.edu>
2015-11-12 11:26:11 -0500

    Add rest of submodules in tokenize to tokenize/mod.rs

--- src/tokenize/mod.rs
+++ src/tokenize/mod.rs
@@ -1,2 +1,11 @@
 pub mod api;
+pub mod casual;
+pub mod mwe;
+pub mod punkt;
+pub mod regexp;
+pub mod sexpr;
+pub mod simple;
+pub mod stanford;
+pub mod texttiling;
+pub mod treebank;
 pub mod util;
